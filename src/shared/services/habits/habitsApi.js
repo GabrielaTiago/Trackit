c73dda4e074035e572d0caa32bd2c061d3c1b479
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-02 15:25:43 -0300

    Restructures the way 'Today' requests are handled

--- src/shared/services/habits/habitsApi.js
+++ src/shared/services/habits/habitsApi.js
@@ -6,6 +6,12 @@ export function getHabits(token) {
   return response;
 }
 
+export function getTodayHabits(token) {
+  const response = api.get("/habits/today", null, token);
+
+  return response;
+}
+
 export function createHabit(body, token) {
   const response = api.post("/habits", body, token);
 
