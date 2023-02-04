f531fe453b9fce462716a880837e5c3922dc17af
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-04 11:25:47 -0300

    Refactoring the location of the array that renders the weekdays

---
+++ src/shared/mock/weekdaysData.js
@@ -0,0 +1,9 @@
+export const weekdaysData = [
+  { id: 0, weekday: "D" },
+  { id: 1, weekday: "S" },
+  { id: 2, weekday: "T" },
+  { id: 3, weekday: "Q" },
+  { id: 4, weekday: "Q" },
+  { id: 5, weekday: "S" },
+  { id: 6, weekday: "S" },
+];
