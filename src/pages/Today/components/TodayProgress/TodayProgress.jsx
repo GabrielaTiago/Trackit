e393908cfcd40775daaee637fcb61ff78bbf5978
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-04 23:21:50 -0300

    Fixes NaN progress error

--- src/pages/Today/components/TodayProgress/TodayProgress.jsx
+++ src/pages/Today/components/TodayProgress/TodayProgress.jsx
@@ -4,7 +4,7 @@ import { Typography } from "./Styles";
 export function TodayProgress() {
   const { progress } = useProgressContext();
 
-  return progress !== 0 ? (
+  return progress > 0 ? (
     <Typography color={true}>{progress}% dos hábitos concluídos</Typography>
   ) : (
     <Typography color={false}>Nenhum hábito concluído ainda</Typography>
